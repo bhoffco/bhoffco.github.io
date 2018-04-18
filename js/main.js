c1287ddf4866cacbb85095306a1e6f8f42cbedcf
bhoffco <bhoffco@gmail.com>
2018-04-18 16:15:49 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+var fixer = document.getElementById('menuOpen');
+var cat = document.getElementById('stick')
+
+fixer.addEventListener('click', fixSlide);
+
+
+/*function fixSlide(){
+  cat.style.height = "700px";
+} */
