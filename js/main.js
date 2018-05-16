07e5c11262feaabdd9389ccc8d9cf761fff5279b
bhoffco <bhoffco@gmail.com>
2018-05-16 16:34:47 -0400

    F

--- js/main.js
+++ js/main.js
@@ -1,12 +1,68 @@
+// initialize firebasebase
 
+let config = {
+    apiKey: "AIzaSyDVHoEWn509Fz48cO5p1FEdGvEt90s9MHQ",
+    authDomain: "level5site-b6a75.firebaseapp.com",
+    databaseURL: "https://level5site-b6a75.firebaseio.com",
+    projectId: "level5site-b6a75",
+    storageBucket: "level5site-b6a75.appspot.com",
+    messagingSenderId: "638940821090"
+  };
+  firebase.initializeApp(config);
 
+//reference leads collected
 
+let messagesRef = firebase.database().ref('leads');
 
-
-var fixer = document.getElementById('openPain');
+// fix for header
+let fixer = document.getElementById('openPain');
 
 fixer.addEventListener('click', fixSlide);
 
 function fixSlide(){
   console.log(123);
+}
+
+// listen for form Submit
+
+document.getElementById('contact-form').addEventListener('submit', submitForm);
+
+//submit form
+
+function submitForm(e){
+  e.preventDefault();
+
+  // get values
+
+  let name = getInputVal('name');
+  let company = getInputVal('company');
+  let email = getInputVal('email');
+  let phone = getInputVal('phone');
+  let message = getInputVal('message');
+
+  //save message
+
+  saveMessage(name, company, email, phone,message);
+
+  // show alert
+
+}
+
+// function to get form values
+
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// save lead info to firebase
+
+function saveMessage(name, company, email, phone, message){
+  let newMessagesRef = messagesRef.push();
+  newMessagesRef.set({
+    name: name,
+    company: company,
+    email: email,
+    phone: phone,
+    message: message
+  });
 }
